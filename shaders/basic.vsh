c4dcf389b58f48e2cfd647290441cced20f8ba93
Vivek <iamwarcode@gmail.com>
2014-07-05 05:06:27 +0530

    mvp

--- shaders/basic.vsh
+++ shaders/basic.vsh
@@ -4,8 +4,11 @@ layout (location = 0) in vec3 position;
 layout (location = 1) in vec3 color;
 
 out vec3 Color;
+uniform mat4 m;
+uniform mat4 v;
+uniform mat4 p;
 
 void main(){
   Color = color;
-  gl_Position = vec4(position.x, -position.y, position.z, 1.0);
+  gl_Position = p*v*m*vec4(position.x, -position.y, position.z, 1.0);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -12,21 +12,48 @@
 #include <mesh.h>
 #include <music.h>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 int main(int argc, char *argv[]){
   Window w(600, 450, "2d game");
   Shader s("../shaders/basic");
   Shader red("../shaders/red");
-  Mesh q;
+  Mesh t;
   Music music("../assets/music/always.mp3");
-
-  q.Sphere();
+  
   music.Play();
+  
+  t.Sphere();
+  
+  glm::mat4 model;
+  glm::mat4 view;
+  glm::mat4 proj;
+  
+  GLint m = glGetUniformLocation(s.program, "m");
+  GLint v = glGetUniformLocation(s.program, "v");
+  GLint p = glGetUniformLocation(s.program, "p");
+  
+  view = glm::lookAt(
+    glm::vec3(0.0f, 0.0f,  4.0f),
+    glm::vec3(0.0f, 0.0f,  0.0f),
+    glm::vec3(0.0f, 1.0f,  0.0f)
+  );
+  
+  
+  proj = glm::perspective(45.0f, 600.0f/450.0f, 1.0f ,10.0f);
   
   while(w.running){
       w.Event();
-      w.Clear(0.5, 0.5, 0.5,1);
+      w.Clear(0, 0.5, 0.5,1);
         s.Use();
-        q.Draw();
+        model = glm::rotate(model, 1.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+        glUniformMatrix4fv(m, 1, GL_FALSE, glm::value_ptr(model));
+        glUniformMatrix4fv(v, 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(p, 1, GL_FALSE, glm::value_ptr(proj));
+        t.Draw();
+        
       w.SwapBuffers();
   }
   
